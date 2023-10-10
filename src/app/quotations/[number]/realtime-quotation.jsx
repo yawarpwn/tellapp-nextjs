0b658f9f4497bf240ed1714402bad7fa003c7bb5
yawarpwn <john.kazz@gmail.com>
2023-10-10 18:24:56 -0500

    add react-pdf/rederer

--- src/app/quotations/[number]/realtime-quotation.jsx
+++ src/app/quotations/[number]/realtime-quotation.jsx
@@ -1,10 +1,84 @@
 'use client'
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
+import PDF from '@/components/pdf/document'
 
-function RealtimeQuotation({ serverQuotation}) {
+function RealtimeQuotation({ serverQuotation }) {
   const [quotation, setQuotation] = useState(serverQuotation)
+  const { number, create_at, items } = quotation
+
+  const formatedDate = new Intl.DateTimeFormat('es').format(create_at)
   return (
-  <pre>{JSON.stringify(quotation, null, 2)}</pre>
+    <>
+      <header>
+        <button className="btn btn-primary">Update</button>
+      </header>
+      <PDF />
+      <div className="container mx-auto px-4 py-8">
+        <div className="rounded-lg shadow overflow-hidden">
+          <div className="px-6 py-4">
+            <h2 className="text-2xl font-bold mb-2">Invoice #{number}</h2>
+            <p className="text-sm">Creado: {formatedDate}</p>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Descripción</th>
+                  <th>Cant</th>
+                  <th>Precio</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {items.map(item => {
+                  return (
+                    <tr key={item.id}>
+                      <td>{item.name}</td>
+                      <td>{item.qty}</td>
+                      <td>$100.00</td>
+                      <td>$100.00</td>
+                    </tr>
+                  )
+                })}
+                <tr>
+                  <td
+                    colSpan={3}
+                    className="text-right py-3 px-4 uppercase font-semibold text-sm"
+                  >
+                    Subtotal:
+                  </td>
+                  <td colSpan="" className="text-right py-3 px-4">
+                    $200.00
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    colSpan={3}
+                    className="text-right py-3 px-4 uppercase font-semibold text-sm"
+                  >
+                    IGV:
+                  </td>
+                  <td colSpan="" className="text-right py-3 px-4">
+                    $20.00
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    colSpan={3}
+                    className="text-right py-3 px-4 uppercase font-semibold text-sm"
+                  >
+                    Total:
+                  </td>
+                  <td colSpan="" className="text-right py-3 px-4">
+                    $220.00
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/header/login-form.jsx
+++ src/components/header/login-form.jsx
@@ -26,13 +26,13 @@ function LoginForm({ session }) {
     return (
       <>
         <div className="dropdown dropdown-end">
-          <label tabindex="0" className="btn btn-ghost btn-circle avatar">
+          <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
               <img src={session.user.user_metadata.picture} />
             </div>
           </label>
           <ul
-            tabindex="0"
+            tabIndex="0"
             className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
           >
             <li>
