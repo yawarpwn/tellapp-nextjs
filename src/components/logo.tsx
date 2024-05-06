f4868351ce007edc83d629476eb27775eb33b208
johneyder <john.kazz@gmail.com>
2024-05-06 18:10:47 -0500

    removing quotations-old

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,16 +4,14 @@ export function Logo({ size }: {
 	size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 }) {
 	return (
-		<Link href='/quotations'>
+		<Link href='/new-quos'>
 			<span
 				className={cn(
 					`
-font-extrabold text-xl inline-flex animate-background-shine 
-bg-[linear-gradient(110deg,rgb(177_75_244),45%,rgb(250_85_95),55%,rgb(177_75_244))] 
-bg-[length:250%_100%] bg-clip-text text-transparent`,
-					{
-						'text-4xl': size === 'xl',
-					},
+          font-extrabold text-xl inline-flex animate-background-shine 
+          bg-[linear-gradient(110deg,rgb(177_75_244),45%,rgb(250_85_95),55%,rgb(177_75_244))] 
+          bg-[length:250%_100%] bg-clip-text text-transparent`,
+					size === 'xl' && 'text-4xl',
 				)}
 			>
 				TELLAPP
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,106 +1,100 @@
 export const QUOTATION_LOCALSTORAGE_NAME = 'TELL_QUO'
 
 import {
-	CustomersIcon,
-	GalleryIcon,
-	PrinterIcon,
-	ProductsIcon,
-	QuotationIcon,
-	SignalIcon,
-	TruckIcon,
+  CustomersIcon,
+  GalleryIcon,
+  PrinterIcon,
+  ProductsIcon,
+  QuotationIcon,
+  SignalIcon,
+  TruckIcon,
 } from '@/icons'
 
 export const TABLES = {
-	Quotations: 'quotations',
-	Products: 'products',
-	Signals: 'signals',
-	Customers: 'customers',
-	Gallery: 'gallery',
+  Quotations: 'quotations',
+  Products: 'products',
+  Signals: 'signals',
+  Customers: 'customers',
+  Gallery: 'gallery',
 } as const
 
 export const ROW_PER_PAGE = 6
 
 export const PRODUCT_CATEGORIES = {
-	CINTAS_SEGURIDAD: 'cintas seguridad',
-	OBRAS: 'obras',
-	PROTECCION_VIAL: 'proteccion vial',
-	FOTOLUMINISCENTE: 'fotoluminiscente',
-	SEGURIDAD: 'seguridad',
-	VIALES: 'viales',
-	VINILES: 'viniles',
-	LUCHA_CONTRA_INCENDIO: 'lucha contra incendio',
-	ARTICULOS_SEGURIDAD: 'articulos seguridad',
-	EPP: 'epp',
-	SERVICIO: 'servicio',
-	ROPA_SEGURIDAD: 'ropa seguridad',
+  CINTAS_SEGURIDAD: 'cintas seguridad',
+  OBRAS: 'obras',
+  PROTECCION_VIAL: 'proteccion vial',
+  FOTOLUMINISCENTE: 'fotoluminiscente',
+  SEGURIDAD: 'seguridad',
+  VIALES: 'viales',
+  VINILES: 'viniles',
+  LUCHA_CONTRA_INCENDIO: 'lucha contra incendio',
+  ARTICULOS_SEGURIDAD: 'articulos seguridad',
+  EPP: 'epp',
+  SERVICIO: 'servicio',
+  ROPA_SEGURIDAD: 'ropa seguridad',
 } as const
 
 export const ITEMS_PER_PAGE = 12
 
 export const NAVIGATION = [
-	{
-		title: 'Cotizaciones',
-		href: '/new-quos',
-		icon: QuotationIcon,
-	},
-	{
-		title: 'Productos',
-		href: '/products',
-		icon: ProductsIcon,
-	},
+  {
+    title: 'Cotizaciones',
+    href: '/new-quos',
+    icon: QuotationIcon,
+  },
+  {
+    title: 'Productos',
+    href: '/products',
+    icon: ProductsIcon,
+  },
 
-	{
-		title: 'Clientes',
-		href: '/customers',
-		icon: CustomersIcon,
-	},
-	{
-		title: 'Rótulos',
-		href: '/labels',
-		icon: PrinterIcon,
-	},
-	{
-		title: 'Agencias',
-		href: '/agencies',
-		icon: TruckIcon,
-	},
+  {
+    title: 'Clientes',
+    href: '/customers',
+    icon: CustomersIcon,
+  },
+  {
+    title: 'Rótulos',
+    href: '/labels',
+    icon: PrinterIcon,
+  },
+  {
+    title: 'Agencias',
+    href: '/agencies',
+    icon: TruckIcon,
+  },
 
-	{
-		title: 'Galeria',
-		href: '/gallery',
-		icon: GalleryIcon,
-	},
-	{
-		title: 'Señales',
-		href: '/signals',
-		icon: SignalIcon,
-	},
-
-	{
-		title: 'Antiguas',
-		href: '/quotations',
-		icon: QuotationIcon,
-	},
+  {
+    title: 'Galeria',
+    href: '/gallery',
+    icon: GalleryIcon,
+  },
+  {
+    title: 'Señales',
+    href: '/signals',
+    icon: SignalIcon,
+  },
 ]
 
 export const GALLERY_CATEGORIES = {
-	SenalesViales: 'Señales Viales',
-	SenalesObras: 'Señales Obras',
-	SenalesConvencionales: 'Señales Convencionales',
-	SenalesFotoluminiscentes: 'Señales fotoluminiscentes',
-	SenalesDecorativas: 'Señales Decorativas',
-	SenalesReflectivas: 'Señales Reflectivas',
-	SenalesMineria: 'Señales Minería',
-	SenalesAcrilico: 'Señales Acrílico',
-	LaminasReflectivas: 'Láminas reflectivas',
-	RotulosStickers: 'Rótulos y stickers',
-	Plantillas: 'Plantillas',
+  SenalesViales: 'Señales Viales',
+  SenalesObras: 'Señales Obras',
+  SenalesConvencionales: 'Señales Convencionales',
+  SenalesFotoluminiscentes: 'Señales fotoluminiscentes',
+  SenalesDecorativas: 'Señales Decorativas',
+  SenalesReflectivas: 'Señales Reflectivas',
+  SenalesMineria: 'Señales Minería',
+  SenalesAcrilico: 'Señales Acrílico',
+  LaminasReflectivas: 'Láminas reflectivas',
+  RotulosStickers: 'Rótulos y stickers',
+  Plantillas: 'Plantillas',
 } as const
 
 export const SIGNALS_CATEGORIES = {
-	viales: 'Viales',
-	seguridad: 'Seguridad',
-	decorativas: 'Decorativas',
-	obras: 'Obras',
-	fotoluminiscentes: 'Fotoluminicentes',
+  viales: 'Viales',
+  seguridad: 'Seguridad',
+  decorativas: 'Decorativas',
+  obras: 'Obras',
+  fotoluminiscentes: 'Fotoluminicentes',
 } as const
