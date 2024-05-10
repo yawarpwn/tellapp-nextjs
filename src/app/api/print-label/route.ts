9bd63ab9f663a162afe7b029e59961b846e67676
johneyder <john.kazz@gmail.com>
2024-05-09 21:05:30 -0500

    fix dev env chromium

--- src/app/api/print-label/route.ts
+++ src/app/api/print-label/route.ts
@@ -1,12 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 import { LabelTemplate } from '@/components/label-template'
-import { chromium } from '@playwright/test'
+import { chromium } from 'playwright'
+
+export const CHROMIUM_EXECUTABLE_PATH =
+  'https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v119.0.0-pack.tar'
+
+export const TAILWIND_CDN =
+  'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
 
 export async function POST(request: NextRequest) {
   try {
     const ReactDOMServer = (await import('react-dom/server')).default
-    const browser = await chromium.launch()
+
+    let browser
+
+    if (process.env.NODE_ENV === 'development') {
+      browser = await chromium.launch()
+    } else if (process.env.NODE_ENV === 'production') {
+      const executablePath = chromium.executablePath()
+      browser = await chromium.launch({
+        executablePath,
+      })
+    }
+
+    if (!browser) {
+      throw new Error('No browser')
+    }
+
     const page = await browser.newPage()
 
     const label = await request.json()
